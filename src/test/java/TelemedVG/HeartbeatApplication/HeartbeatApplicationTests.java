d462154dccf0f926aecbf57b2fbc360ead86e88c
krist <vujnovic.kristina1@gmail.com>
2022-04-05 11:34:26 +0200

    Heartbeat 1 commit

---
+++ src/test/java/TelemedVG/HeartbeatApplication/HeartbeatApplicationTests.java
@@ -0,0 +1,13 @@
+package TelemedVG.HeartbeatApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeartbeatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
