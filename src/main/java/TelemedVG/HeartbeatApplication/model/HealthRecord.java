6a82ba0a0d1a71a06dc756b431e3b0d8c20fea9a
Dario Jakovljević <jakovljevic.dario@gmail.com>
2022-04-06 19:42:46 +0200

    HealthRecord added - not ready

---
+++ src/main/java/TelemedVG/HeartbeatApplication/model/HealthRecord.java
@@ -0,0 +1,19 @@
+package TelemedVG.HeartbeatApplication.model;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+public class HealthRecord {
+    private int id;
+    private Date dateOfInput;
+    private int systole;
+    private int diastole;
+    private int heartBeat;
+    private String comments;
+
+}
