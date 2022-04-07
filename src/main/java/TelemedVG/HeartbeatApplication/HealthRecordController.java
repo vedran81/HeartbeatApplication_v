a5770e4009a0498ffb475a99032d5d42af017580
MS <smladic@outlook.com>
2022-04-07 09:32:57 +0200

    "NASTAVAK RADA NA KONTROLERU OD HEALTH KARTONA I SPAJANJU HTML-A S ISTIM"

--- src/main/java/TelemedVG/HeartbeatApplication/HealthRecordController.java
+++ src/main/java/TelemedVG/HeartbeatApplication/HealthRecordController.java
@@ -2,6 +2,7 @@ package TelemedVG.HeartbeatApplication;
 
 import TelemedVG.HeartbeatApplication.model.HealthRecord;
 import TelemedVG.HeartbeatApplication.model.HealthRecordRepository;
+import org.springframework.ui.Model;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,8 +19,9 @@ public class HealthRecordController {
 
 
     @GetMapping("/add")
-    public String add(HealthRecord patientHealthRecordForm) {
+    public String add(Model model, HealthRecord patientHealthRecordForm) {
+        model.addAttribute("healthRecordForm", new HealthRecord());
         repository.save(patientHealthRecordForm);
-        return "redirect:/LISTA SA ZAPISIMA";
+        return "redirect:/add_health_record";
     }
 }
