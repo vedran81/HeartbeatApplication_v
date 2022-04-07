7f02d0e9393da0a3d9c9939075ac750c1d4c7544
krist <vujnovic.kristina1@gmail.com>
2022-04-07 10:00:05 +0200

    kwiki no comment 2

--- src/main/java/TelemedVG/HeartbeatApplication/AppUserController.java
+++ src/main/java/TelemedVG/HeartbeatApplication/AppUserController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/app_user")
+@RequestMapping("app_user")
 public class AppUserController {
 
     @Autowired
@@ -18,13 +18,13 @@ public class AppUserController {
     @GetMapping("/list")
     public String list(Model model) {
         model.addAttribute("user", repository.findAll());
-        model.addAttribute("newUser", new AppUser());
 
-        return "app_user";
+        return "app_user/list";
     }
 
     @GetMapping("/add")
-    public String add(AppUser userForm) {
+    public String add(AppUser userForm, Model model) {
+        model.addAttribute("newUser", new AppUser());
 
         repository.save(userForm);
         return "redirect:/app_user/list";
