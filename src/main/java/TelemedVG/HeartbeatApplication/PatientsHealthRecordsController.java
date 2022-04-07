3470892cee8946b123ce958d56c8a7e1b523007d
Dario Jakovljević <jakovljevic.dario@gmail.com>
2022-04-07 16:42:35 +0200

    fixed PatientsHealthRecordsController + patient_information_entry.html

--- src/main/java/TelemedVG/HeartbeatApplication/PatientsHealthRecordsController.java
+++ src/main/java/TelemedVG/HeartbeatApplication/PatientsHealthRecordsController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("add_health_record")
 public class PatientsHealthRecordsController {
 
     @Autowired
