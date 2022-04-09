887a424b2cce7956948399886e3ce9a25e389f92
MS <smladic@outlook.com>
2022-04-09 22:48:56 +0200

    "FIXES"

--- src/main/java/TelemedVG/HeartbeatApplication/TelemedLoginPageController.java
+++ src/main/java/TelemedVG/HeartbeatApplication/TelemedLoginPageController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("landing_page")
 public class TelemedLoginPageController {
+
     @GetMapping("/showLoginPage")
     public String showLoginPage() {
 
